249e9fafbf566c23fae4cd18e6ff68499095ce51
Matheus Fonseca de Souza <matheus.fonseca@contabilizei.com.br>
2022-08-08 16:05:29 -0300

    app

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    console.log("componente antes de ser montado");
+    console.log(this.$el)
+  },
+  mounted() {
+    console.log("componente montado");
+    console.log(this.$el)
+  },
+  beforeUnmount() {
+    console.log("componente antes de ser desmontado");
+    console.log(this.$el)
+  },
+  unmounted() {
+    console.log("componente desmontado");
+    console.log(this.$el)
+  },
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 45px;
+  background: #8c11bd;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <TheHeader v-if="headerIsTrue" />
+  <main>
+    <button @click="headerIsTrue = !headerIsTrue">change state of header</button>
+  </main>
+</template>
+
+<script>
+import TheHeader from "./TheHeader.vue";
+export default {
+  components: { TheHeader },
+  data() {
+    return {
+        headerIsTrue: true
+    }
+  },
+};
+</script>
+
+<style scoped>
+main {
+  width: 100%;
+}
+</style>
